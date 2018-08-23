d0d878949780604bb1eb8b13ec4383e5542d8b35
Frédéric Thomachot <frederic.thomachot@campus-numerique-in-the-alps.com>
2018-08-23 14:20:52 +0200

    recuperation API

--- TransportsCommun/Program.cs
+++ TransportsCommun/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
 
 namespace TransportsCommun
 {
@@ -6,7 +9,27 @@ namespace TransportsCommun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Public transport");
+
+            string urlApi = "https://data.metromobilite.fr/api/linesNear/json?x=5.72792&y=45.18549&dist=800&details=true";
+
+            WebRequest request = WebRequest.Create(urlApi);
+            WebResponse response = request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            Console.WriteLine(responseFromServer);
+
+            JArray json = JArray.Parse(responseFromServer);
+
+
+
+
+
+
         }
     }
 }
